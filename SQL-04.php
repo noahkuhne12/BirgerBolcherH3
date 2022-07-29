9591fd2b17c8770e1f8d4f1d9e382c7b8d4baf0c
noahkuhne12 <50324963+noahkuhne12@users.noreply.github.com>
2022-07-29 10:47:30 +0200

    sql-04 complidt

--- SQL-04.php
+++ SQL-04.php
@@ -1,12 +1,3 @@
 <h1 class="text-center">SQL-04</h1>
 <?php
 include('SQL-04/1.php');
-include('SQL-04/2.php');
-include('SQL-04/3.php');
-include('SQL-04/4.php');
-include('SQL-04/5.php');
-include('SQL-04/6.php');
-include('SQL-04/7.php');
-include('SQL-04/8.php');
-include('SQL-04/9.php');
-include('SQL-04/10.php');
--- SQL-04/1.php
+++ SQL-04/1.php
@@ -1,2 +1,54 @@
-<h3></h3>
+<?php
+    $sql = "SELECT product.ID, product.Name, product.Weight, product.CommodityPrice
+        FROM product 
+        ORDER BY product.ID ASC;"
+    ;
+    $stat = $conn->prepare($sql);
+    $stat->execute();
+    $result = $stat->get_result();
+?>
+
+<div class="card border-secondary mb-3">
+        <div class="card-header">
+            <h2>Opg. 1 - Udskriv alle informationer om alle bolcher</h2>
+        </div>
+        <div class="table-responsive">
+          <table class="table table-hover mb-0">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Navn</th>
+                <th scope="col">Vægt (g)</th>
+                <th scope="col">Råvarepris (øre)</th>
+                <th scope="col">Nettopris (øre)</th>
+                <th scope="col">Salgspris Inkl. moms (øre)</th>
+                <th scope="col">Salgspris 100g Inkl. moms (øre)</th>
+              </tr>
+            </thead>
+            <tbody>
+<?php
+while($row = $result->fetch_assoc()){    
+?>
+              <tr>
+                <th scope="row"><?= $row["ID"] ?></th>
+                <td><?= $row["Name"] ?></td>
+                <td><?= $row["Weight"] ?></td>
+                <td><?= $row["CommodityPrice"] ?></td>
+                <td><?= $netto = round($row["CommodityPrice"]*2.5,2) ?></td>
+                <td><?= $sale = round($netto*1.25,2)?></td>
+                <td><?= round(($sale / $row["CommodityPrice"]) * 100, 2) ?></td>
+              </tr>
+<?php
+}
+?>
+            </tbody>
+          </table>
+        </div>
+        <div class="card-footer bg-transparent border-success">
+            <h3>
+                <?=$sql?>
+            </h3>
+          </div>
+      </div>
+
 
